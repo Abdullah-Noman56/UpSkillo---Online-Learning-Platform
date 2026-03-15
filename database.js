4363245ebb0d729f94f1ca6f0d633a15362613f1
Abdullah-Noman56 <abdullahshaikh565656@gmail.com>
2026-03-15 11:20:02 +0500

    Change database connection password
    Updated database connection password for security.

--- database.js
+++ database.js
@@ -4,7 +4,7 @@ var mysql = require('mysql2');
 var connection = mysql.createConnection({
   host: "localhost", 
   user: "root",      
-  password: "abdullah", 
+  password: "_", // change it according to your SQL server password 
   database: "up_skillo_db"  // Changed to your database
 });
 
